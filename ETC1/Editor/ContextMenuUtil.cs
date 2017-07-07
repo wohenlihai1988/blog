ef4e0e535fa740f02ee93e6741ef9645f227d37d
liuhuan <wohenlihai1988@163.com>
2017-07-07 15:18:15 +0800

    // add etc1 tools

---
+++ ETC1/Editor/ContextMenuUtil.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace ETC1Helper
+{
+    public class ContextMenuUtil 
+    {
+        [MenuItem("Tools/SplitAlpha")]
+        [MenuItem("Assets/ETC1Helper/SplitAlpha", false, 0)]
+        static void SplitAlpha()
+        {
+            var obj = Selection.activeObject;
+            var path = AssetDatabase.GetAssetPath(obj);
+            var go = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)) as GameObject;
+            if (null == go)
+            {
+                EditorUtility.DisplayDialog("error ", "please select a atlas prefab ", "OK");
+                return;
+            }
+            var atlas = go.GetComponent<UIAtlas>();
+            if (null == atlas)
+            {
+                EditorUtility.DisplayDialog("error ", "please select a atlas prefab ", "OK");
+                return;
+            }
+            ETC1Tools.GenerateAlphaResources(atlas);
+        }
+
+        static List<UIAtlas> CollectAllAtlas()
+        {
+            var instanceId = Selection.activeInstanceID;
+            AssetDatabase.GetAssetPath(instanceId);
+            var path = AssetDatabase.GetAssetPath(instanceId);
+            var array = AssetDatabase.LoadAllAssetsAtPath(path);
+            var items = new List<string>();
+            var atlasList = new List<UIAtlas>();
+            LoopAllItems(path, items);
+            foreach (var item in items)
+            {
+                if (item.EndsWith(".prefab"))
+                {
+                    var go = AssetDatabase.LoadAssetAtPath(item, typeof(GameObject)) as GameObject;
+                    if (null == go)
+                    {
+                        Debug.LogError("go is null !! at " + item);
+                        continue;
+                    }
+                    var atlas = go.GetComponent<UIAtlas>();
+                    if(null != atlas)
+                    {
+                        atlasList.Add(atlas);
+                    }
+                    else
+                    {
+                        Debug.LogError("can't find UIAtlas in " + item);
+                    }
+                }
+            }
+            return atlasList;
+        }
+
+        [MenuItem("Assets/ETC1Helper/SwitchToIOS", false, 0)]
+        static void BuildIOS()
+        {
+            var list = CollectAllAtlas();
+            foreach (var atlas in list)
+            {
+                var instanceID = atlas.gameObject.GetInstanceID();
+                var path = AssetDatabase.GetAssetPath(instanceID);
+                var normalMatPath = path.Replace(".prefab", ".mat");
+                var texPath = path.Replace(".prefab", ".png");
+                if (!File.Exists(normalMatPath))
+                {
+                    Debug.LogError("NANI ?? !!!");
+                }
+                else
+                {
+                    ETC1Tools.SetAtlasToIOSSetting(atlas);
+                }
+            }
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem("Assets/ETC1Helper/SwitchToAndroid", false, 0)]
+        static void BuildAndroid()
+        {
+            var list = CollectAllAtlas();
+            var i = 0;
+            foreach(var atlas in list)
+            {
+                if(null == atlas)
+                {
+                    Debug.LogError("null atlas " + i); 
+                    continue;
+                }
+                var instanceID = atlas.gameObject.GetInstanceID();
+                var path = AssetDatabase.GetAssetPath(instanceID); 
+                var alphaMatPath = path.Replace(".prefab", ETC1Tools.AlphaAttachString + ".mat");
+                if (!File.Exists(alphaMatPath))
+                {
+                    ETC1Tools.GenerateAlphaResources(atlas);
+                }
+                else
+                {
+                    ETC1Tools.SetAtlasToAlphaSetting(atlas);
+                }
+                i++;
+            }
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+        }
+
+        static void LoopAllItems(string dir_, List<string> items_)
+        {
+            var dirs = Directory.GetDirectories(dir_);
+            foreach(var dir in dirs)
+            {
+                LoopAllItems(dir, items_);
+            }
+            var items = Directory.GetFiles(dir_);
+            foreach(var item in items)
+            {
+                items_.Add(item); 
+            }
+        }
+    }
+}
---
+++ ETC1/Editor/ETC1Tools.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+namespace ETC1Helper
+{
+    public class ETC1Tools 
+    {
+        public const string AlphaAttachString = "_alpha";
+
+        public static void GenerateAlphaResources(UIAtlas atlas_)
+        {
+            try
+            {
+                var instanceID = atlas_.gameObject.GetInstanceID();
+                var path = AssetDatabase.GetAssetPath(instanceID);
+                var tex = SplitAlphaTexture(atlas_.spriteMaterial.mainTexture as Texture2D);
+                CreateAlphaMaterial(atlas_.spriteMaterial, tex, path.Replace(".prefab", AlphaAttachString + ".mat"));
+                SetAtlasToAlphaSetting(atlas_);
+
+                AssetDatabase.Refresh();
+                AssetDatabase.SaveAssets();
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+                Debug.LogError("atlas " + atlas_.gameObject.name);
+            }
+        }
+
+        public static void CreateAlphaMaterial(Material srcMat_, Texture alphaTex_, string path_)
+        {
+            var alphaMat = new Material(srcMat_);
+            alphaMat.shader = Shader.Find("Unlit/Transparent Colored Split Alpha");
+            alphaMat.SetTexture("_AlphaTex", alphaTex_);
+            AssetDatabase.CreateAsset(alphaMat, path_);
+        }
+
+        public static void SetAtlasToAlphaSetting(UIAtlas atlas_)
+        {
+            var instanceID = atlas_.gameObject.GetInstanceID();
+            var path = AssetDatabase.GetAssetPath(instanceID);
+            var alphaMatPath = path.Replace(".prefab", AlphaAttachString + ".mat");
+            var mainTexPath = path.Replace(".prefab", ".png");
+            var alphaMat = AssetDatabase.LoadAssetAtPath(alphaMatPath, typeof(Material)) as Material;
+            if(null == alphaMat)
+            {
+                Debug.LogError("can't find alpha texture at " + alphaMatPath);
+                return;
+            }
+            atlas_.spriteMaterial = alphaMat;
+            var mainTexImporter = (TextureImporter.GetAtPath(mainTexPath) as TextureImporter);
+            ApplyAndroidTextSetting(mainTexImporter);
+        }
+
+        public static void SetAtlasToIOSSetting(UIAtlas atlas_)
+        {
+            var instanceID = atlas_.gameObject.GetInstanceID();
+            var path = AssetDatabase.GetAssetPath(instanceID);
+            var mormalMatPath = path.Replace(".prefab", ".mat");
+            var mainTexPath = path.Replace(".prefab", ".png");
+            var normalMat = AssetDatabase.LoadAssetAtPath(mormalMatPath, typeof(Material)) as Material;
+            if (null == normalMat)
+            {
+                Debug.LogError("can't find alpha texture at " + mormalMatPath);
+                return;
+            }
+            atlas_.spriteMaterial = normalMat;
+            var mainTexImporter = (TextureImporter.GetAtPath(mainTexPath) as TextureImporter);
+            ApplyIOSTexSetting(mainTexImporter);
+        }
+
+        public static Texture2D SplitAlphaTexture(Texture2D tex)
+        {
+            var path = AssetDatabase.GetAssetPath(tex.GetInstanceID());
+            (TextureImporter.GetAtPath(path) as TextureImporter).isReadable = true;
+            AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            var colArray = tex.GetPixels();
+            var alphaTex = new Texture2D(tex.width, tex.height, TextureFormat.RGBA32, false);
+            var alphaArray = new Color[colArray.Length];
+            for (int i = 0; i < alphaArray.Length; i++)
+            {
+                alphaArray[i].r = alphaArray[i].g = alphaArray[i].b = colArray[i].a;
+                alphaArray[i].a = 1;
+            }
+            alphaTex.SetPixels(alphaArray);
+            var bytes = alphaTex.EncodeToPNG();
+            var srcimporter = (TextureImporter.GetAtPath(path) as TextureImporter);
+            ApplyAndroidTextSetting(srcimporter);
+            var alphaPath = path.Replace(".png", AlphaAttachString).Replace(".psd", AlphaAttachString).Replace(".jpg", AlphaAttachString);
+            alphaPath += ".png";
+            File.WriteAllBytes(alphaPath, bytes);
+            AssetDatabase.Refresh();
+            var importer = (TextureImporter.GetAtPath(alphaPath) as TextureImporter);
+            ApplyAndroidTextSetting(importer);
+            AssetDatabase.ImportAsset(alphaPath, ImportAssetOptions.ForceUpdate);
+            return AssetDatabase.LoadAssetAtPath(alphaPath, typeof(Texture2D)) as Texture2D;
+        }
+
+        public static void ApplyAndroidTextSetting(TextureImporter importer_)
+        {
+            if (null != importer_)
+            {
+                importer_.textureType = TextureImporterType.GUI;
+                importer_.textureFormat = TextureImporterFormat.ETC_RGB4;
+                importer_.mipmapEnabled = false;
+                importer_.isReadable = false;
+
+                importer_.SetPlatformTextureSettings("Android", 4096, importer_.textureFormat);
+                AssetDatabase.ImportAsset(importer_.assetPath, ImportAssetOptions.ForceUpdate);
+            }
+        }
+
+        public static void ApplyIOSTexSetting(TextureImporter importer_)
+        {
+            if (null != importer_)
+            {
+                importer_.textureType = TextureImporterType.GUI;
+                importer_.textureFormat = TextureImporterFormat.ARGB32;
+                importer_.mipmapEnabled = false;
+                importer_.isReadable = false;
+
+                importer_.SetPlatformTextureSettings("iPhone", 4096, importer_.textureFormat);
+                AssetDatabase.ImportAsset(importer_.assetPath, ImportAssetOptions.ForceUpdate);
+            }
+        }
+    }
+}
